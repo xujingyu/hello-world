358a4ed052cb403f4ddeb0e357f5409b553954a0
xujingyu <yeyu1899@163.com>
2016-08-06 20:05:21 +0800

    secondary commit

---
+++ src/com/jftt/Main.java
@@ -0,0 +1,10 @@
+package com.jftt;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("你好苏州");
+        System.out.println("你好无锡");
+    }
+}
